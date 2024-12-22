4fcfad28646cda25884d71bd5b2960dd70bed12d
Eng-MaysQasem <mbqasem7@gmail.com>
2024-12-22 13:56:49 +0200

    Session 1: Initial setup and components

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Student from './Componanets/Student/Student.componannt';
+
+
+function App() {
+ 
+
+  return (
+    <>
+    
+      <h1>Welcome to GSG course</h1>
+      <Student/>
+      <Student/>
+
+
+ 
+    </>
+  )
+}
+
+export default App;
---
+++ src/Componanets/Student/Student.componannt.tsx
@@ -0,0 +1,13 @@
+import'./Student.css'
+const Student = ()=>{
+    return (
+    <div className='std-wrapper'>
+  
+    <h3>Mays Qasem</h3git init
+>
+    <hr/>
+    </div>)
+  
+  };
+
+  export default Student;
