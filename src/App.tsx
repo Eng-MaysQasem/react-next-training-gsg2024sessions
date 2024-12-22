9ddf4d0e3d85fc7ed21b68364c7a8b59a028b126
Eng-MaysQasem <mbqasem7@gmail.com>
2024-12-22 15:32:09 +0200

    Session 2: Added Props and Data Binding example with array of strings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Student from './Componanets/Student/Student.componannt';
+const CoursesList=['React','HTML','CSS'];
 
 
 function App() {
@@ -10,9 +11,9 @@ function App() {
     <>
     
       <h1>Welcome to GSG course</h1>
-      <Student name="Mays Qasem" age={23} isGraduate={true}/>
-      <Student name="Areej Qasem"age={21}isGraduate={false}/>
-      <Student name="Sama Qasem"age={18-2}isGraduate={!true}/>
+      <Student name="Mays Qasem" age={23} isGraduate={true} list={CoursesList}/>
+      <Student name="Areej Qasem"age={21}isGraduate={false}list={CoursesList}/>
+      <Student name="Sama Qasem"age={18-2}isGraduate={!true}list={CoursesList}/>
 
 
  
--- src/Componanets/Student/Student.componannt.tsx
+++ src/Componanets/Student/Student.componannt.tsx
@@ -1,14 +1,17 @@
+import CoursesList from '../CoursesList/CoursesList.Componants';
 import'./Student.css'
 interface IProps{
   name:string;
   age:number;
   isGraduate:boolean;
+  list:string[]
 }
 const Student = (props:IProps)=>{
     return (
     <div className='std-wrapper'>
 
     { "StudentName:"+props.name.toLocaleUpperCase() +"//" + "age:"+props.age+"//"+"IsGraduated:"+props.isGraduate.toString()}
+    <CoursesList list={props.list} />
 
     <hr/>
     </div>)
